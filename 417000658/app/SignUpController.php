fca9cf1c42232587daabfdcd0cb5aa988adf9df7
BotLaneBanter <lordjosephhewitt@hotmail.com>
2020-10-21 15:20:53 -0400

    Added successful signup message...sort of

--- 417000658/app/SignUpController.php
+++ 417000658/app/SignUpController.php
@@ -27,95 +27,95 @@ class SignUpController extends Controller{
         //Check if the register button was clicked
         if(isset($_POST['SignUp'])){
 
-        //Create the controllers
-        $loginController = new LoginController();
+            //UNUSED
+            //Create the controllers
+            $loginController = new LoginController();
 
-        //Create models
-       // $loginModel = new LoginModel();
+            //Create models
+            // $loginModel = new LoginModel();
 
-        //Create and set the different views
-        //Login
-        $loginView = new View();
-        $loginView->setTemplate(TPL_DIR . '/login.tpl.php');
+            //Create and set the different views
+            //Login
+            $loginView = new View();
+            $loginView->setTemplate(TPL_DIR . '/login.tpl.php');
 
-        //Set controller views
-        $loginController->setView($loginView);
-
-        
-
-        //Create new validae object
-        $validate = new Validate();
-
-
-        $userRecord = $this->observableModel->getRecord($_POST['email']);
-        $hashedPassword = "";
-        $hashedPassword = $userRecord['users']['Password'];
-
-        //Get user record
-        $userData = $this->observableModel->getRecord($_POST['email']);
-
-        $validate->isEmailValid($_POST['email']);
-        $validate->isPasswordValid($_POST['password']);
+            //Set controller views
+            $loginController->setView($loginView);
+            //UNUSED
             
-            //Check the full name isn't empty
-            if(empty($_POST['formFullName'])){
-                $fullNameError['Errors'] = "Please Enter Your Name";
-                $this->view->addVar("Errors", $fullNameError);
-                $errorExists = true;
-            }
-            //Check a valid email was entered
-            else if($validate->getErrorEmailThrown()){
-                $emailError['Errors'] = "Invalid Email";
-                $this->view->addVar("Errors", $emailError);
-                $errorExists = true;
-            }
-            //Check a valid password was entered
-            else if($validate->getErrorPasswordThrown()){
-                $passwordError['Errors'] = "Invalid Password";
-                $this->view->addVar("Errors", $passwordError);
-                $errorExists = true;
-            }
-            //Check the password matches the retyped password
-            else if(($_POST['password']) != ($_POST['retypedPassword'])){
-                $mismatchError['Errors'] = "Passwords Do Not Match";
-                $this->view->addVar("Errors", $mismatchError);
-                $errorExists = true;
-            }
-            //Check the terms checkbox was ticked
-            else if(!isset($_POST['termsCheckbox'])){
-                $termsError['Errors'] = "Please Read And Accept The Terms";
-                $this->view->addVar("Errors", $termsError);
-                $errorExists = true;
-            }
-            //Check the users email isn't already in use
-            else if($_POST['email'] == $userData['users']['Email']){
-                $termsError['Errors'] = "Account Already Exists";
-                $this->view->addVar("Errors", $termsError);
-                $errorExists = true;
-            }
-            //If successful, run the loginController
-            else if( (!$validate->getErrorEmailThrown()) && (!$validate->getErrorPasswordThrown())){
 
-                //Successful Sign Up Message
-                $successfulSignUp['successfulSignUp'] = "Sign Up Successful. Please login below";
-                $loginView->addVar("successfulSignUp", $successfulSignUp);
+            //Create new validae object
+            $validate = new Validate();
 
-                $this->observableModel->storeUserData($_POST['formFullName'], $_POST['email'], $_POST['password']);
 
-                //FOR NOW WILL NEED TO ADD THE NEW USER EMAILS MANUALLY TO THE ACCESSIBLE ARRAY IN SessionClass.php
+            $userRecord = $this->observableModel->getRecord($_POST['email']);
+            $hashedPassword = "";
+            $hashedPassword = $userRecord['users']['Password'];
 
-                header("Location: login.php");
-                //require "login.php";
+            //Get user record
+            $userData = $this->observableModel->getRecord($_POST['email']);
 
-            }
+            $validate->isEmailValid($_POST['email']);
+            $validate->isPasswordValid($_POST['password']);
                 
-            if($errorExists == true){
-                //Tell the model to update the data which changed
-                $this->observableModel->updateChangedData($data);  
+                //Check the full name isn't empty
+                if(empty($_POST['formFullName'])){
+                    $fullNameError['Errors'] = "Please Enter Your Name";
+                    $this->view->addVar("Errors", $fullNameError);
+                    $errorExists = true;
+                }
+                //Check a valid email was entered
+                else if($validate->getErrorEmailThrown()){
+                    $emailError['Errors'] = "Invalid Email";
+                    $this->view->addVar("Errors", $emailError);
+                    $errorExists = true;
+                }
+                //Check a valid password was entered
+                else if($validate->getErrorPasswordThrown()){
+                    $passwordError['Errors'] = "Invalid Password";
+                    $this->view->addVar("Errors", $passwordError);
+                    $errorExists = true;
+                }
+                //Check the password matches the retyped password
+                else if(($_POST['password']) != ($_POST['retypedPassword'])){
+                    $mismatchError['Errors'] = "Passwords Do Not Match";
+                    $this->view->addVar("Errors", $mismatchError);
+                    $errorExists = true;
+                }
+                //Check the terms checkbox was ticked
+                else if(!isset($_POST['termsCheckbox'])){
+                    $termsError['Errors'] = "Please Read And Accept The Terms";
+                    $this->view->addVar("Errors", $termsError);
+                    $errorExists = true;
+                }
+                //Check the users email isn't already in use
+                else if($_POST['email'] == $userData['users']['Email']){
+                    $termsError['Errors'] = "Account Already Exists";
+                    $this->view->addVar("Errors", $termsError);
+                    $errorExists = true;
+                }
+                //If successful, run the loginController
+                else if( (!$validate->getErrorEmailThrown()) && (!$validate->getErrorPasswordThrown())){
+
+                    //Successful Sign Up Message
+                    $successfulSignUp['successfulSignUp'] = "Sign Up Successful. Please login below";
+
+                    $this->observableModel->storeUserData($_POST['formFullName'], $_POST['email'], $_POST['password']);
+
+                    //FOR NOW WILL NEED TO ADD THE NEW USER EMAILS MANUALLY TO THE ACCESSIBLE ARRAY IN SessionClass.php
+                    $successMessage = http_build_query($successfulSignUp);
+                    header("Location: login.php" . "?" . $successMessage);
+                    //require "login.php";
+
+                }
                     
-                //Tell the model to notify it's attached observers, pushing the updated data to them
-                $this->observableModel->notify();
-            }
+                if($errorExists == true){
+                    //Tell the model to update the data which changed
+                    $this->observableModel->updateChangedData($data);  
+                        
+                    //Tell the model to notify it's attached observers, pushing the updated data to them
+                    $this->observableModel->notify();
+                }
 
         }//End of POST check
         else{
